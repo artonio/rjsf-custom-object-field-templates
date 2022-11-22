a905215c3679b9dfd53b4f84d2a3929829ae227b
Anton <anton.sukhovatkin@rmp.uhn.ca>
2022-11-22 11:53:28 -0500

    primereact lib

---
+++ packages/react-jsf-custom-object-templates/src/lib/primereact/Theme/Theme.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { Button } from "primereact/button";
+
+import ArrayFieldTemplate from "../ArrayFieldTemplate";
+import ErrorList from "../ErrorList";
+import Fields from "../Fields";
+import FieldTemplate from "../FieldTemplate";
+import ObjectFieldTemplate from "../ObjectFieldTemplate";
+import Widgets from "../Widgets";
+
+import {getDefaultRegistry, ThemeProps} from "@rjsf/core";
+import {Templates} from "@rjsf/antd";
+
+
+const { fields, widgets } = getDefaultRegistry();
+
+const DefaultChildren = (
+  <Button className="mt-3" type="submit" label="Submit" />
+);
+
+// const Theme: ThemeProps = {
+//   // children: DefaultChildren,
+//   ArrayFieldTemplate,
+//   fields: { ...fields, ...Fields },
+//   FieldTemplate,
+//   ObjectFieldTemplate,
+//   ErrorList,
+//   widgets: { ...widgets, ...Widgets },
+// };
+//
+// export default Theme;
+const Theme: ThemeProps = {
+  templates: Templates,
+  widgets: Widgets,
+};
+
+export default Theme;
